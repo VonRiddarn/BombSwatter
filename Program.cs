8d39e0ca4dda2108b5a629c8301d670f0db67d23
VonRiddarn <vonriddarn@gmail.com>
2023-11-14 10:32:57 +0100

    fix: removed ASCII test code

--- Program.cs
+++ Program.cs
@@ -6,13 +6,6 @@ internal class Program
 {
 	static void Main(string[] args)
 	{
-
-		Board board = new Board(15, 15, 20);
-		board.TestStartGame();
-
-		Debug.WriteLine(board.ToString());
-
-
 		using var game = new VonRiddarn.BombSwatter.Game1();
 		game.Run();
 
